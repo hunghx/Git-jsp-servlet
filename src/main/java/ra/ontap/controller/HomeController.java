14bb5f4179248300dfa0562d9ef6a313a2ae94ea
hunghx <baby10051999@gmail.com>
2024-10-03 15:23:18 +0700

    up

--- src/main/java/ra/ontap/controller/HomeController.java
+++ src/main/java/ra/ontap/controller/HomeController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/home")
+    @RequestMapping("/home") // khai báo đường dẫn xử lí
     public String home(){
-       return "home" ;
+       return "home" ; // chọn tên view để trả về
     }
     @RequestMapping
     public String index(){
